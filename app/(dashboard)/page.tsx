9daab8b9a139a39bc97ab632871a37875df3de3a
dotarjun <71163609+dotarjun@users.noreply.github.com>
2024-05-27 12:07:34 +0530

    chore: organise
    Signed-off-by: dotarjun <71163609+dotarjun@users.noreply.github.com>

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant={"destructive"}>Hello world</Button>
+    </div>
+  );
+}
