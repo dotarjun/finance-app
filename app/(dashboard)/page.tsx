bc4d489aa02973b512d377414b3a5db332eb97f1
dotarjun <71163609+dotarjun@users.noreply.github.com>
2024-05-27 12:28:33 +0530

    chore: init clerk auth
    Signed-off-by: dotarjun <71163609+dotarjun@users.noreply.github.com>

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button variant={"destructive"}>Hello world</Button>
+      <p>This is an authenticated route</p>
     </div>
   );
 }
