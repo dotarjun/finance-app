1a6a4639920e7fae13f5a059de14aa6b40e40a8b
dotarjun <71163609+dotarjun@users.noreply.github.com>
2024-05-28 23:22:35 +0530

    feat: build sign in page
    Signed-off-by: dotarjun <71163609+dotarjun@users.noreply.github.com>

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,29 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2e2A47]">Welcome Back!</h1>
+          <p className="text-base text-[#7e8ca0]">
+            Log in or Create Account to get to the dashboard
+          </p>
+        </div>
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignIn />
+          </ClerkLoaded>
+        </div>
+      </div>
+      <div className="h-full bg-brand-contrast-400 hidden lg:flex items-center justify-center">
+        <Image src={"/logo.png"} height={500} width={500} alt="logo" />
+      </div>
+    </div>
+  );
 }
