5fe81cb0dd259d509f9c5eb47a20b2cf65928d99
dotarjun <71163609+dotarjun@users.noreply.github.com>
2024-05-28 23:26:56 +0530

    copy: update copy
    Signed-off-by: dotarjun <71163609+dotarjun@users.noreply.github.com>

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
         <div className="text-center space-y-4 pt-16">
           <h1 className="font-bold text-3xl text-[#2e2A47]">Welcome Back!</h1>
           <p className="text-base text-[#7e8ca0]">
-            Log in or Create Account to get to the dashboard
+            Log in to get to your dashboard
           </p>
         </div>
         <div className="flex items-center justify-center mt-8">
