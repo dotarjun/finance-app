a0cc69c6d085d710e1679bd6c26292cdd6e034a3
dotarjun <71163609+dotarjun@users.noreply.github.com>
2024-05-30 23:50:51 +0530

    feat: start mobile nav
    Signed-off-by: dotarjun <71163609+dotarjun@users.noreply.github.com>

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,8 +1,12 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
+import { useMedia } from "react-use";
 
 import { NavButton } from "./nav-button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Button } from "./ui/button";
 
 const routes = [
   {
@@ -28,7 +32,30 @@ const routes = [
 ];
 
 export const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const pathname = usePathname();
+  const router = useRouter();
+  const isMobile = useMedia("(max-width: 1024px)", false);
+
+  const onClick = (href: string) => {
+    router.push(href);
+    setIsOpen(false);
+  };
+
+  if (isMobile) {
+    return (
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger>
+          <Button
+            variant={"outline"}
+            size={"sm"}
+            className="font-normal bg-white/10 hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent"
+          ></Button>
+        </SheetTrigger>
+      </Sheet>
+    );
+  }
 
   return (
     <nav className="hidden lg:flex items-center gap-x-2">
