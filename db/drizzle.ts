2514fcd34404b05d8c228d15d685e90b09b2a078
dotarjun <71163609+dotarjun@users.noreply.github.com>
2024-06-03 22:44:23 +0530

    db: try setting up drizzle with supabase
    Signed-off-by: dotarjun <71163609+dotarjun@users.noreply.github.com>

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -8,3 +8,15 @@ const connectionString = process.env.DATABASE_URL!;
 // Disable prefetch as it is not supported for "Transaction" pool mode
 export const client = postgres(connectionString, { prepare: false });
 export const db = drizzle(client);
+
+const main = async () => {
+
+    
+    try {
+        await
+    } catch (error) {
+        console.error("Error during Migration", error)
+        process.exit(1);
+    }
+
+}
