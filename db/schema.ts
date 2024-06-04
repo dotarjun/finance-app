f591d3e40809044a7cdf190e47145feb1e69268e
dotarjun <71163609+dotarjun@users.noreply.github.com>
2024-06-04 22:05:14 +0530

    db: add plaidId
    Signed-off-by: dotarjun <71163609+dotarjun@users.noreply.github.com>

--- db/schema.ts
+++ db/schema.ts
@@ -8,6 +8,7 @@ import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
+  plaidId: text("plaid_id"),
   name: text("name").notNull(),
   userId: text("user_id").notNull(),
 });
