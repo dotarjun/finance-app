9365c4982360727860031b30f22416d25ca0a23a
dotarjun <71163609+dotarjun@users.noreply.github.com>
2024-06-06 23:55:26 +0530

    remove logs
    Signed-off-by: dotarjun <71163609+dotarjun@users.noreply.github.com>

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -4,16 +4,12 @@ import { db } from "@/db/drizzle";
 import { accounts } from "@/db/schema";
 
 const app = new Hono().get("/", async (c) => {
-  console.log("running accounts query");
-
   const data = await db
     .select({
       id: accounts.id,
       name: accounts.name,
     })
     .from(accounts);
-
-  console.log("ran accounts query");
   return c.json({ hi: "hi" });
 });
 
