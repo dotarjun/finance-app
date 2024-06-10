df9c86209ffec820699678b38c667c888d281c6f
dotarjun <71163609+dotarjun@users.noreply.github.com>
2024-06-11 00:12:27 +0530

    schema: create insert account schema
    Signed-off-by: dotarjun <71163609+dotarjun@users.noreply.github.com>

--- db/schema.ts
+++ db/schema.ts
@@ -1,10 +1,5 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
-
-// export const users = pgTable("users", {
-//   id: serial("id").primaryKey(),
-//   fullName: text("full_name"),
-//   phone: varchar("phone", { length: 256 }),
-// });
+import { pgTable, text } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
@@ -12,3 +7,5 @@ export const accounts = pgTable("accounts", {
   name: text("name").notNull(),
   userId: text("user_id").notNull(),
 });
+
+export const insertAccountSchema = createInsertSchema(accounts);
