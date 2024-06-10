e065b4f78277780aaa69bfe69eb5b4acca41981c
dotarjun <71163609+dotarjun@users.noreply.github.com>
2024-06-10 13:00:44 +0530

    feat: add clerk middleware to all /api/accounts routes
    Signed-off-by: dotarjun <71163609+dotarjun@users.noreply.github.com>

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,10 +1,31 @@
 import { Hono } from "hono";
-import { clerkMiddleware } from "@hono/clerk-auth";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { HTTPException } from "hono/http-exception";
 
 import { db } from "@/db/drizzle";
 import { accounts } from "@/db/schema";
 
-const app = new Hono().get("/", clerkMiddleware(), async (c) => {
+const app = new Hono();
+
+const protectedPathsRegex: string = "*";
+
+app.use(
+  protectedPathsRegex,
+  clerkMiddleware({
+    publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!,
+    secretKey: process.env.CLERK_SECRET_KEY!,
+  })
+);
+
+app.get("/", async (c) => {
+  const auth = getAuth(c);
+
+  if (!auth?.userId) {
+    throw new HTTPException(401, {
+      res: c.json({ error: "unauthorized" }, 401),
+    });
+  }
+
   const data = await db
     .select({
       id: accounts.id,
