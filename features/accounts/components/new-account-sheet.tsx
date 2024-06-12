a02fce979bc768301b9a0564f6fbfe2c4e1979f2
dotarjun <71163609+dotarjun@users.noreply.github.com>
2024-06-13 03:08:07 +0530

    util: add open close logic
    Signed-off-by: dotarjun <71163609+dotarjun@users.noreply.github.com>

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,14 +1,18 @@
+import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts";
+
 import {
   Sheet,
   SheetContent,
   SheetDescription,
   SheetHeader,
   SheetTitle,
-} from "../../../components/ui/sheet";
+} from "@/components/ui/sheet";
 
 export const NewAccountSheet = () => {
+  const { isOpen, onClose } = useNewAccount();
+
   return (
-    <Sheet open>
+    <Sheet open={isOpen} onOpenChange={onClose}>
       <SheetContent className="space-y-4">
         <SheetHeader>
           <SheetTitle>New Account</SheetTitle>
