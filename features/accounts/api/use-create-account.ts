ac9d1b2dfccbe360d0d21f80cebed50cfabd721f
dotarjun <71163609+dotarjun@users.noreply.github.com>
2024-06-15 21:20:33 +0530

    feat: create account api
    Signed-off-by: dotarjun <71163609+dotarjun@users.noreply.github.com>

---
+++ features/accounts/api/use-create-account.ts
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+
+import { client } from "@/lib/hono";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<typeof client.api.accounts.$post>;
+type RequestType = InferRequestType<typeof client.api.accounts.$post>["json"];
+
+export const useCreateAccount = () => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const res = await client.api.accounts.$post({ json });
+      return await res.json();
+    },
+    onSuccess: () => {
+      toast.success("Account Created");
+      queryClient.invalidateQueries({ queryKey: ["accounts"] });
+    },
+    onError: () => {
+      toast.error("Failed to create an Account");
+    },
+  });
+
+  return mutation;
+};
