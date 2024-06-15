ad70f6d62e2c4fc771d65b923ed6f6e59232390c
dotarjun <71163609+dotarjun@users.noreply.github.com>
2024-06-15 21:22:24 +0530

    feat: add an new account form to the sidebar
    Signed-off-by: dotarjun <71163609+dotarjun@users.noreply.github.com>

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,5 +1,6 @@
-import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts";
+import { z } from "zod";
 
+import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts";
 import {
   Sheet,
   SheetContent,
@@ -7,9 +8,21 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
+import { insertAccountSchema } from "@/db/schema";
+
+import { AccountsForm } from "./account-form";
 
 export const NewAccountSheet = () => {
   const { isOpen, onClose } = useNewAccount();
+  const formSchema = insertAccountSchema.pick({
+    name: true,
+  });
+
+  type FormValues = z.input<typeof formSchema>;
+
+  const onSubmit = (values: FormValues) => {
+    console.log(values);
+  };
 
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
@@ -20,6 +33,11 @@ export const NewAccountSheet = () => {
             Create a new account to track your Xpenses
           </SheetDescription>
         </SheetHeader>
+        <AccountsForm
+          onSubmit={onSubmit}
+          disabled={false}
+          defautValues={{ name: "" }}
+        />
       </SheetContent>
     </Sheet>
   );
