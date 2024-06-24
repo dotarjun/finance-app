fe3d76b2584035d345e85870471558874a5f21db
dotarjun <71163609+dotarjun@users.noreply.github.com>
2024-06-24 10:26:34 +0530

    feat: add dynamic filtering
    Signed-off-by: dotarjun <71163609+dotarjun@users.noreply.github.com>

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -4,10 +4,38 @@ import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts";
 
 import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { DataTable } from "@/components/data-table";
+
+import { Payment, columns } from "./columns";
+
+const data: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "a@example.com",
+  },
+];
+
+export async function DemoPage() {
+  return <div className="container mx-auto py-10"></div>;
+}
 
 const AccountsPage = () => {
   const newAccount = useNewAccount();
+
+  const filterBy = "email";
+
+  const changeFilter = () => {
+    return <div></div>;
+  };
 
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -19,6 +47,9 @@ const AccountsPage = () => {
             Add new
           </Button>
         </CardHeader>
+        <CardContent>
+          <DataTable columns={columns} data={data} filterBy={filterBy} />
+        </CardContent>
       </Card>
     </div>
   );
